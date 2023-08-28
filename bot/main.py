4e4fa102fdf5fb75f850a8b7d61146e54e7f36a5
Bobbadillio <kelly634@umn.edu>
2023-08-27 22:26:37 -0500

    Adjusting belts and removing lichess tracking, which is now handled by another bot.

--- bot/main.py
+++ bot/main.py
@@ -26,8 +26,7 @@ BELT_COLORS = ["Black", "Red", "Brown", "Purple", "Blue", "Green", "Orange", "Ye
 
 CHESS_COM_BELTS = [
     (2400, "Black"),
-    (2200, "Red"),
-    (2000, "Brown"),
+    (2100, "Red"),
     (1800, "Purple"),
     (1600, "Blue"),
     (1400, "Green"),
@@ -36,34 +35,22 @@ CHESS_COM_BELTS = [
     (0,    "White")
 ]
 
-LICHESS_BELTS = [
-    (2400, "Black"),
-    (2250, "Red"),
-    (2100, "Brown"),
-    (2000, "Purple"),
-    (1800, "Blue"),
-    (1600, "Green"),
-    (1400, "Orange"),
-    (1200, "Yellow"),
-    (0,    "White")
-]
+# LICHESS_BELTS = [
+#     (2400, "Black"),
+#     (2250, "Red"),
+#     (2100, "Brown"),
+#     (2000, "Purple"),
+#     (1800, "Blue"),
+#     (1600, "Green"),
+#     (1400, "Orange"),
+#     (1200, "Yellow"),
+#     (0,    "White")
+# ]
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-UNIMPLEMENTED_COMMANDS = """
-.profile
-To view your profile. To check others profile -> .profile @user
-.rank
-Displays your current rank in the server (based on your lichess/chess.com rating).
-.page
-Displays players close to your rank
-.top
-verfication (Automatic)
-Challenge a person and paste the invite link in any channel :D (lichess only)
-.progress
-To check your progress"""
 
 # @bot.command()
 # async def embed(ctx):
@@ -131,84 +118,33 @@ to your Discord ID ({author}). You can change the location back after your profi
 
 @bot.command()
 async def lichess(ctx, *args):
-    """link a lichess account with discord user ID in bio. usage: !lichess LichessUsername"""
-    ### steps:
-    # Validate input
-    author = str(ctx.author)
-    if len(args)==0:
-        await ctx.send(f"Please specify a lichess account with your discord user ID({author}) in bio. usage: !lichess LichessUsername")
-        return
-
-    username = args[0]
-
-    # Create the discord user if it doesn't exist
-    EnsureDiscordAuthorExists(author)
-    try:
-        pg = Postgres(DATABASE_URL)
-        retrieved_lichess_profiles = pg.query("SELECT * from lichess_profiles WHERE discord_id = %s", (author,))
-
-        # Get the lichess profile and
-        client = berserk.Client()
-        profile = client.users.get_public_data(username)
-
-        if len(retrieved_lichess_profiles) == 0:
-            # first time handshake
-            bio = profile.get("profile", dict()).get("bio", "")
-            if author not in bio:
-                await ctx.send(f"Verification failed. Your lichess profile must have your Discord ID ({author}) in your bio. \
-You can change the location back after your profile is linked.")
-                return
-
-        # insert or update lichess data if rating is stable
-        await update_lichess(ctx, profile, quiet=False)
-        # Update belt if profile was inserted or updated
-        await update_belt(ctx, author)
-
-    except berserk.exceptions.ResponseError as e:
-        logging.error(f"{e}")
-        await ctx.send(f"request to link {username} failed. This error has been logged.")
-    except Exception as e:
-        logging.error(f"{e}")
-        await ctx.send(f"request to link {username} failed. This error has been logged.")
+    await ctx.send("Bottrosian doesn't currently track lichess accounts.")
 
 @bot.command()
 async def update(ctx):
-    """Updates your lichess and chess.com ratings, and recalculates your belt. usage: !update"""
+    """Updates your chess.com ratings, and recalculates your belt. usage: !update"""
     pg = Postgres(DATABASE_URL)
     author = str(ctx.author)
 
-    profile_result = pg.query("""SELECT GREATEST(awarded_belt, chesscom_belt, lichess_belt) AS belt, 
-    chesscom_username, last_chesscom_elo AS chesscom_elo, lichess_username, last_lichess_elo AS lichess_elo FROM authenticated_users 
+    profile_result = pg.query("""SELECT GREATEST(awarded_belt, chesscom_belt) AS belt, 
+    chesscom_username, last_chesscom_elo AS chesscom_elo FROM authenticated_users 
     NATURAL LEFT JOIN chesscom_profiles 
-    NATURAL LEFT JOIN lichess_profiles 
     NATURAL LEFT JOIN mod_profiles 
     WHERE discord_id = %s""", (author,))
     old_belt = profile_result[0][0]
-
-    #update lichess
-    retrieved_lichess_profiles = pg.query("SELECT lichess_username, lichess_belt AS old_belt from lichess_profiles WHERE discord_id = %s", (author,))
-    if len(retrieved_lichess_profiles)>0:
-        # perform handshake and perform first time insert
-        username = retrieved_lichess_profiles[0][0]
-        old_belt = retrieved_lichess_profiles[0][1]
-        client = berserk.Client()
-        profile = client.users.get_public_data(username)
-        await update_lichess(ctx, profile, old_belt)
 
     #update chesscom
     retrieved_chesscom_profiles = pg.query("SELECT chesscom_username, chesscom_belt AS old_belt from chesscom_profiles WHERE discord_id = %s", (author,))
     if len(retrieved_chesscom_profiles)>0:
         # perform handshake and perform first time insert
         username = retrieved_chesscom_profiles[0][0]
-        old_belt = retrieved_lichess_profiles[0][1]
         await update_chesscom(ctx, author, username, old_belt)
 
     #update belt
     await update_belt(ctx, author)
-    profile_result = pg.query("""SELECT GREATEST(awarded_belt, chesscom_belt, lichess_belt) AS belt, 
-    chesscom_username, last_chesscom_elo AS chesscom_elo, lichess_username, last_lichess_elo AS lichess_elo FROM authenticated_users 
+    profile_result = pg.query("""SELECT GREATEST(awarded_belt, chesscom_belt) AS belt, 
+    chesscom_username, last_chesscom_elo AS chesscom_elo FROM authenticated_users 
     NATURAL LEFT JOIN chesscom_profiles 
-    NATURAL LEFT JOIN lichess_profiles 
     NATURAL LEFT JOIN mod_profiles 
     WHERE discord_id = %s""", (author,))
     new_belt = profile_result[0][0]
@@ -234,7 +170,7 @@ async def unlink(ctx, *args):
                 await ctx.send(f"""User {ctx.author} {"not " if lichess_deletion == 0 else ""}unlinked from lichess""")
         elif args[0] == "chess":
                 chesscom_deletion = pg.query("""DELETE FROM chesscom_profiles WHERE discord_id = %s RETURNING *""", (author,))
-                await ctx.send(f"""User {ctx.author} {"not " if chesscom_deletion == 0 else ""}unlinked from lichess""")
+                await ctx.send(f"""User {ctx.author} {"not " if chesscom_deletion == 0 else ""}unlinked from chess.com""")
         else:
             await ctx.send(f"""thanks {ctx.author}, but your message tried to unlink you from '{args[0]}'
             which is invalid. please try !unlink !unlink chess or !unlink lichess""")
@@ -250,27 +186,23 @@ async def profile(ctx, *args):
         discord_id_lookup = args[0]
 
 
-    profile_result = pg.query("""SELECT discord_id AS discord, GREATEST(awarded_belt, chesscom_belt, lichess_belt) AS belt, 
-    chesscom_username, last_chesscom_elo AS chesscom_elo, lichess_username, last_lichess_elo AS lichess_elo FROM authenticated_users 
+    profile_result = pg.query("""SELECT discord_id AS discord, GREATEST(awarded_belt, chesscom_belt) AS belt, 
+    chesscom_username, last_chesscom_elo AS chesscom_elo FROM authenticated_users 
     NATURAL LEFT JOIN chesscom_profiles 
-    NATURAL LEFT JOIN lichess_profiles 
     NATURAL LEFT JOIN mod_profiles 
     WHERE discord_id = %s""", (discord_id_lookup,))
 
     if len(profile_result)==0:
-        await ctx.send(f"discord user {discord_id_lookup} doesn't appear to be linked to an account. Please try the !lichess or !chess commands.")
+        await ctx.send(f"discord user {discord_id_lookup} doesn't appear to be linked to an account. Please try the !chess command.")
         return
 
     message_to_send = []
-    discord_id, belt, chesscom_username, chesscom_rapid, lichess_username, lichess_classical = profile_result[0]
+    discord_id, belt, chesscom_username, chesscom_rapid = profile_result[0]
     message_to_send.append(f"Discord ID: {discord_id}")
     message_to_send.append(f"Belt: {belt}")
     if chesscom_username is not None:
         rating = "provisional" if chesscom_rapid is None else chesscom_rapid
         message_to_send.append(f"Chess.com Rapid: {rating}")
-    if lichess_username is not None:
-        rating = "provisional" if lichess_classical is None else lichess_classical
-        message_to_send.append(f"Lichess Classical: {rating}")
 
     final_message = '\n'.join(message_to_send)
     await ctx.send(f"{final_message} ")
@@ -279,16 +211,11 @@ async def profile(ctx, *args):
 
 @bot.command()
 async def top(ctx):
-    """this command returns a list of the top 10 rated players in the dojo based on chess.com rapid and lichess classical. usage: !top"""
+    """this command returns a list of the top 10 rated players in the dojo based on chess.com rapid. usage: !top"""
     pg = Postgres(DATABASE_URL)
     chesscom_top_headers = ["chess.com username", "chess.com rapid"]
     chesscom_results = pg.query("""select chesscom_username as username, last_chesscom_elo as elo from chesscom_profiles where last_chesscom_elo IS NOT NULL order by elo desc limit 10;""")
     await ctx.send(f"```{tabulate.tabulate(chesscom_results, headers=chesscom_top_headers)}```\n ")
-
-    lichess_top_headers = ["lichess username", "lichess classical"]
-    lichess_results = pg.query(
-        """select lichess_username as username, last_lichess_elo as elo from lichess_profiles where last_lichess_elo IS NOT NULL order by elo desc limit 10; ;""")
-    await ctx.send(f"```{tabulate.tabulate(lichess_results,headers=lichess_top_headers)}```\n ")
 
 @bot.command()
 async def pgn(ctx, *args):
@@ -352,25 +279,7 @@ def is_super_user(author):
     return any([each_role.name in SUPER_ROLES for each_role in author.roles])
 
 async def update_lichess(ctx, profile, old_belt=None, quiet = True):
-    pg = Postgres(DATABASE_URL)
-    if all([value.get("prov",False) for value in profile.get("perfs", dict()).values()]):
-        await ctx.send(
-            f"{str(ctx.author)} does not have any stable ratings on lichess. Have you played enough recent games on lichess for a stable rating?")
-        return
-
-    classical = profile.get("perfs", dict()).get("classical", dict())
-    classical_rating = None
-    if not classical.get("prov",False):
-        classical_rating = classical.get("rating", None)
-    mapped_belt = lichess_to_belt(classical_rating)
-    username = profile.get("id", None)
-    author = str(ctx.author)
-    pg.query("""INSERT INTO lichess_profiles (lichess_username, discord_id, last_lichess_elo, previous_lichess_elo, lichess_belt)
-        VALUES (%s, %s, %s, %s, %s) ON CONFLICT (lichess_username) DO UPDATE SET 
-        last_lichess_elo = EXCLUDED.last_lichess_elo, lichess_belt = EXCLUDED.lichess_belt;
-        """, (username, author, classical_rating, classical_rating, mapped_belt))
-    if not quiet:
-        await ctx.send(f"Lichess user successfully linked! Based on lichess classical rating, {username} has a {mapped_belt.lower()} belt")
+    await ctx.send(f"Lichess data is no longer tracked by Bottrosian")
 
 async def update_chesscom(ctx, author, username, old_belt=None, quiet = True):
     pg = Postgres(DATABASE_URL)
@@ -398,17 +307,16 @@ async def setbelt(ctx, color):
         await ctx.send(f"No such role {color} Belt. Please pick one of: {' '.join(BELT_COLORS)}")
         return
 
-    old_belts = [each_role for each_role in member.roles if "Belt" in each_role.name and each_role.name!=f"{color} Belt"]
-    if len(old_belts)>0:
-        await member.remove_roles(*old_belts)
+    # old_belts = [each_role for each_role in member.roles if "Belt" in each_role.name and each_role.name!=f"{color} Belt"]
+    # if len(old_belts)>0:
+    #     await member.remove_roles(*old_belts)
     await member.add_roles(retrieved)
 
 async def update_belt(ctx, discord_id):
     pg = Postgres(DATABASE_URL)
-    highest_belt = pg.query("""SELECT GREATEST(awarded_belt, chesscom_belt, lichess_belt) AS belt FROM authenticated_users 
+    highest_belt = pg.query("""SELECT GREATEST(awarded_belt, chesscom_belt) AS belt FROM authenticated_users 
         NATURAL LEFT JOIN mod_profiles 
-        NATURAL LEFT JOIN chesscom_profiles 
-        NATURAL LEFT JOIN lichess_profiles 
+        NATURAL LEFT JOIN chesscom_profiles
          WHERE discord_id = %s;""", (discord_id, ))
     await setbelt(ctx, highest_belt[0][0])
 
@@ -416,14 +324,6 @@ def chess_com_to_belt(rating):
     if rating is None:
         return "White"
     for (threshold, name) in CHESS_COM_BELTS:
-        if rating > threshold:
-            return name
-    return "White"
-
-def lichess_to_belt(rating):
-    if rating is None:
-        return "White"
-    for (threshold, name) in LICHESS_BELTS:
         if rating > threshold:
             return name
     return "White"
